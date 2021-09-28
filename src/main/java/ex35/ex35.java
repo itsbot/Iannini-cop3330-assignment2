89de1e53b06e57989b68e01bd9841806c3297907
itsbot <patrickiannini@knights.ucf.edu>
2021-09-27 23:10:38 -0400

    Completed Exercise 35

---
+++ src/main/java/ex35/ex35.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 34 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package ex35;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var names = new ArrayList<String>();
+        String in;
+        while (true) {
+            System.out.print("Enter a name: ");
+            in = sc.nextLine();
+            if (in.equals("")) {
+                break;
+            }
+            names.add(in);
+        }
+        Random r = new Random();
+        String winner =  names.get(r.nextInt(names.size()));
+        System.out.println(winner);
+    }
+}
